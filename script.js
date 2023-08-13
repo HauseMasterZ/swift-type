5d56664f8377aefde0d033e02d38c9a10b169d5a
HauseMasterZ <113833707+HauseMasterZ@users.noreply.github.com>
2023-08-14 02:50:44 +0530

    Add Quote Length

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let timerInterval = null;
 let words = currentQuote.split(' ');
 let errorQuote = "";
 let typedValue = "";
+let interval;
 let endOfWord = false;
 let currentWordIndex = 0;
 const quoteDisplay = document.getElementById("quote");
@@ -49,7 +50,8 @@ function refreshQuote() {
         errorsDisplay.textContent = "Errors: 0";
         categoryDisplay.textContent = "";
     } else {
-        // endTest();
+        clearInterval(interval);
+        resultImg.setAttribute('src', '');
         resultImg.classList.add('hidden');
         document.body.style.backgroundColor = '#fff';
         clearInterval(timerInterval);
@@ -59,11 +61,29 @@ function refreshQuote() {
 
 }
 
+
+const quoteLengthRadios = document.getElementsByName("quoteLength");
+
+
 function fetchRandomQuote() {
-    fetch("https://api.quotable.io/random")
+    let minLength = 0;
+    let maxLength = 0;
+    if (quoteLengthRadios[1].checked) {
+      minLength = 1;
+      maxLength = 100;
+    } else if (quoteLengthRadios[2].checked) {
+      minLength = 100;
+      maxLength = 250;
+    } else if (quoteLengthRadios[3].checked) {
+      minLength = 250;
+        maxLength = 430;
+    }
+
+    const url = minLength > 0 ? `https://api.quotable.io/quotes/random/?minLength=${minLength}&maxLength=${maxLength}` : "https://api.quotable.io/quotes/random";
+    fetch(url)
         .then(response => response.json())
         .then(data => {
-            currentQuote = data.content;
+            currentQuote = data[0]['content'];
             quoteDisplay.textContent = currentQuote;
             words = currentQuote.split(' ');
             inputBox.value = "";
@@ -87,49 +107,6 @@ function fetchRandomQuote() {
     startTime = 0; // Reset the start time
 }
 
-// function checkInput() {
-//     let typedValue = inputBox.value;
-//     let typedWords = typedValue.split(' ');
-//     // typedWords = typedWords.filter(word => word !== ''); // Remove whitespace elements
-//     const latestWord = typedWords[typedWords.length - 1];
-//     if (typedValue === currentQuote || (typedWords.length >= words.length && typedValue.endsWith('.'))) {
-//         inputBox.disabled = true;
-//         endTest();
-//         refreshButton.focus();
-//         return;
-//     }
-//     if (startTime == 0) {
-//         startTimer();
-//     }
-//     currentWordIndex = typedValue.trim().split(' ').length - 1;
-//     // currentWordIndex = currentWordIndex.filter(word => word !== '').length - 1;
-//     console.log(typedWords, currentWordIndex);
-//     let currentWord = words[currentWordIndex];
-//     let error = false;
-//     if (latestWord === currentWord.slice(0, latestWord.length)) {
-//         if (typedValue.endsWith(' ')) {
-//             if (typedWords[typedWords.length - 2].length < currentWord.length) {
-//                 errorQuote[currentWordIndex] = `<span class="error">${currentWord}</span>`;
-//                 quoteDisplay.innerHTML = errorQuote.join(' ');
-//                 error = true;
-//             }
-//         }
-//         else {
-//             errorQuote[currentWordIndex] = `<span class="correct">${currentWord}</span>`;
-//             quoteDisplay.innerHTML = errorQuote.join(' ');
-//             error = false;
-//         }
-//     } else {
-//         errorQuote[currentWordIndex] = `<span class="error">${currentWord}</span>`;
-//         quoteDisplay.innerHTML = errorQuote.join(' ');
-//         error = true;
-//     }
-//     totalTyped = typedValue.length;
-//     totalErrors = error ? totalErrors + 1 : totalErrors;
-//     const accuracy = calculateAccuracy(totalTyped, totalErrors);
-//     accuracyDisplay.textContent = `Accuracy: ${accuracy}%`;
-//     errorsDisplay.textContent = `Errors: ${totalErrors}`;
-// }
 
 function checkInput(event) {
     typedValue = inputBox.value;
@@ -147,24 +124,25 @@ function checkInput(event) {
     const latestWord = typedWords[typedWords.length - 1];
     const currentWord = words[typedWords.length - 1];
     const isEndOfQuote = typedWords.length >= words.length && typedValue.endsWith('.');
-    const isCorrect = latestWord === currentWord.slice(0, latestWord.length);
     if (typedValue === currentQuote || isEndOfQuote) {
-        inputBox.disabled = true;
         endTest();
         refreshButton.focus();
         return;
     }
-
     if (startTime === 0) {
         startTimer();
     }
-
-    if (isCorrect && !(endOfWord && latestWord.length < currentWord.length)) {
-        errorQuote[typedWords.length - 1] = `<span class="correct">${currentWord}</span>`;
-    } else {
-        errorQuote[typedWords.length - 1] = `<span class="error">${currentWord}</span>`;
+    try {
+        const isCorrect = latestWord === currentWord.slice(0, latestWord.length);
+        if (isCorrect && !(endOfWord && latestWord.length < currentWord.length)) {
+            errorQuote[typedWords.length - 1] = `<span class="correct">${currentWord}</span>`;
+        } else {
+            errorQuote[typedWords.length - 1] = `<span class="error">${currentWord}</span>`;
+        }
+        totalErrors = isCorrect ? totalErrors : totalErrors + 1;
+    } catch (e) {
+        return;
     }
-    totalErrors = isCorrect ? totalErrors : totalErrors + 1;
 
     quoteDisplay.innerHTML = errorQuote.join(' ');
 
@@ -187,7 +165,7 @@ function updateTimer() {
 
 function displaySpeed(prefix, number, stars) {
     let count = 0;
-    const interval = setInterval(() => {
+    interval = setInterval(() => {
         categoryDisplay.textContent = `${prefix} ${count}km/h ${stars}`;
         count++;
         if (count > number) {
@@ -198,50 +176,53 @@ function displaySpeed(prefix, number, stars) {
 
 function endTest() {
     clearInterval(timerInterval);
+    inputBox.disabled = true;
     endTime = new Date().getTime();
-    const wpm = calculateWPM(endTime);
     const netWPM = calculateNetWPM(totalErrors, endTime);
+    const wpm = calculateWPM(endTime);
     const accuracy = calculateAccuracy(totalTyped, totalErrors);
     grossWPMDisplay.textContent = `Gross WPM: ${wpm}`;
     netWPMDisplay.textContent = `Net WPM: ${netWPM}`;
     accuracyDisplay.textContent = `Accuracy: ${accuracy}%`;
     errorsDisplay.textContent = `Errors: ${totalErrors}`;
     if (netWPM < 50) {
-        displaySpeed('Sloth-paced Typist 🦥', (netWPM / 10) * 2, '⭐');
         resultImg.setAttribute('src', 'svg/sloth.svg');
+        displaySpeed('Sloth-paced Typist 🦥', (netWPM / 10) * 2, '⭐');
         document.body.style.backgroundColor = '#C69061';
     } else if (netWPM >= 50 && netWPM < 60) {
-        displaySpeed('Turtle-paced Typist 🐢', Math.random() * (50 - 10) + 10, '⭐');
         resultImg.setAttribute('src', 'svg/sea_turtle.svg');
+        displaySpeed('Turtle-paced Typist 🐢', Math.random() * (50 - 10) + 10, '⭐');
         document.body.style.backgroundColor = 'blue';
     } else if (netWPM >= 60 && netWPM < 70) {
-        displaySpeed('Horse-speed Typist 🐎', Math.random() * (90 - 50) + 50, '⭐⭐');
         resultImg.setAttribute('src', 'svg/horse.svg');
+        displaySpeed('Horse-speed Typist 🐎', Math.random() * (90 - 50) + 50, '⭐⭐');
         document.body.style.backgroundColor = '#BFAA87';
     } else if (netWPM >= 70 && netWPM < 80) {
-        displaySpeed('Lion-fingered Typist 🦁', Math.random() * (120 - 90) + 90, '⭐⭐');
         resultImg.setAttribute('src', 'svg/lion.svg');
+        displaySpeed('Lion-fingered Typist 🦁', Math.random() * (120 - 90) + 90, '⭐⭐');
         document.body.style.backgroundColor = '#DD8547';
     } else if (netWPM >= 80 && netWPM < 90) {
-        displaySpeed('Cheetah-swift Typist 🐆', Math.random() * (180 - 120) + 120, '⭐⭐⭐');
         resultImg.setAttribute('src', 'svg/cheetah.svg');
+        displaySpeed('Cheetah-swift Typist 🐆', Math.random() * (180 - 120) + 120, '⭐⭐⭐');
         document.body.style.backgroundColor = '#DC864B';
     } else if (netWPM >= 90 && netWPM < 100) {
-        displaySpeed('Eagle-eyed Typist 🦅', Math.random() * (300 - 180) + 180, '⭐⭐⭐⭐');
         resultImg.setAttribute('src', 'svg/eagle.svg');
+        displaySpeed('Eagle-eyed Typist 🦅', Math.random() * (300 - 180) + 180, '⭐⭐⭐⭐');
         document.body.style.backgroundColor = '#AB7D5A';
     } else if (netWPM >= 100 && netWPM < 140) {
-        displaySpeed('Falcon-keyed Typist 🦅', Math.random() * (400 - 300) + 300, '⭐⭐⭐⭐⭐');
         resultImg.setAttribute('src', 'svg/falcon.svg');
+        displaySpeed('Falcon-keyed Typist 🦅', Math.random() * (400 - 300) + 300, '⭐⭐⭐⭐⭐');
         document.body.style.backgroundColor = 'lightblue';
     } else if (netWPM >= 140 && netWPM < 150) {
-        displaySpeed('Supersonic Typist 🚀 AKA HauseMaster 🟥🟧🟩', Math.random() * (1000 - 300) + 300, '⭐⭐⭐⭐⭐');
         resultImg.setAttribute('src', 'svg/hausemaster.svg');
+        displaySpeed('Supersonic Typist 🚀 AKA HauseMaster', Math.random() * (1000 - 300) + 300, '⭐⭐⭐⭐⭐');
         document.body.style.backgroundColor = 'Red';
     } else {
         displaySpeed('Lightning-Fast Typist ⚡️', 300000, '⭐⭐⭐⭐⭐');
-        document.body.style.backgroundColor = 'lightyellow';
+        document.body.style.backgroundColor = 'yellow';
+        return;
     }
+
     resultImg.classList.remove('hidden');
     resultImg.classList.add('slide-in');
 }
