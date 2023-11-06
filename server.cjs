abfab9bf7a1e50ad7e003625da4448137a919467
HauseMasterZ <113833707+HauseMasterZ@users.noreply.github.com>
2023-11-07 00:04:42 +0530

    Add rate limiting

--- server.cjs
+++ server.cjs
@@ -1,7 +1,17 @@
 const express = require('express');
 const path = require('path');
+const rateLimit = require('express-rate-limit');
 const app = express();
 const port = 3000;
+
+// Set up rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+
+// Apply rate limiting to all requests
+app.use(limiter);
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'src/')));
@@ -14,5 +24,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-
-
